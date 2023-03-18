804cb98f1ef566b8fe313e8d7315481ada7cb618
Andrew Senichkin <Kniws10@yandex.ru>
2023-03-18 19:37:01 +0300

    Make edits to the code style

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,7 +12,7 @@ function Card(card) {
                 <h2 className="element__title">{card.name}</h2>
                 <div className="element__container-like">
                     <button className="element__smile" type="button"></button>
-                    <p className="element__count-likes"></p>
+                    <p className="element__count-likes">{card.likes.length}</p>
                 </div>
             </div>
         </article>
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,11 +3,11 @@ import profilePen from '../image/VectorPen.svg';
 import api from '../utils/Api';
 import Card from './Card';
 
-function Main({onEditProfile, onAddPlace, onEditAvatar, onCardClick}) {
+function Main({ handleEditProfileClick, handleAddPlaceClick, handleEditAvatarClick, onCardClick }) {
     const [userName, setUserName] = React.useState("");
     const [userDescription, setUserDescription] = React.useState("");
     const [userAvatar, setUserAvatar] = React.useState("");
-    const [cards, getInitialCards] = React.useState([])
+    const [cards, setInitialCards] = React.useState([])
 
     React.useEffect(() => {
         api.getAboutUserInfo().then((propsUserInfo) => {
@@ -15,9 +15,9 @@ function Main({onEditProfile, onAddPlace, onEditAvatar, onCardClick}) {
             setUserDescription(propsUserInfo.about)
             setUserAvatar(propsUserInfo.avatar)
         })
-        .catch((err) => console.log(`Ошибка: ${err}`))
+            .catch((err) => console.log(`Ошибка: ${err}`))
         api.getInitialCards().then((cardData) => {
-            getInitialCards(
+            setInitialCards(
                 cardData.map((data) => ({
                     likes: data.likes,
                     name: data.name,
@@ -26,41 +26,41 @@ function Main({onEditProfile, onAddPlace, onEditAvatar, onCardClick}) {
                 }))
             )
         })
-        .catch((err) => console.log(`Ошибка:${err}`));
+            .catch((err) => console.log(`Ошибка:${err}`));
     }, [])
-        
+
     return (
-<main className="content">
-        <section className="profile">
-            <div className="profile__container">
-                <div className="profile__wrapper">
-                    <img className="profile__avatar" src={userAvatar} alt="Жак-Ив Кусто"/>
-                    <button className="profile__edit-avatar"  type="button" onClick={() => onEditAvatar(true)}>
-                        <img className="profile__edit-pen" src={profilePen} alt="Письменная ручка"/>
-                    </button>
-                </div>
-                <div className="profile__info">
-                    <div className="profile__container-title">
-                        <h1 className="profile__name">{userName}</h1>
-                        <button className="profile__edit-button" onClick={() => onEditProfile(true)} type="button"></button>
+        <main className="content">
+            <section className="profile">
+                <div className="profile__container">
+                    <div className="profile__wrapper">
+                        <img className="profile__avatar" src={userAvatar} alt="Изображение профиля, пользователя" />
+                        <button className="profile__edit-avatar" type="button" onClick={() => handleEditAvatarClick(true)}>
+                            <img className="profile__edit-pen" src={profilePen} alt="Письменная ручка" />
+                        </button>
                     </div>
-                    <p className="profile__more">{userDescription}</p>
+                    <div className="profile__info">
+                        <div className="profile__container-title">
+                            <h1 className="profile__name">{userName}</h1>
+                            <button className="profile__edit-button" onClick={() => handleEditProfileClick(true)} type="button"></button>
+                        </div>
+                        <p className="profile__more">{userDescription}</p>
+                    </div>
                 </div>
-            </div>
-            <button className="profile__add-button" onClick={() => onAddPlace(true)} type="button"></button>
-        </section>
-        <section className="elements">
-            {cards.map((card) => (
-                <Card
-                    key={card.cardId}
-                    likes={card.likes}
-                    name={card.name}
-                    link={card.link}
-                    onCardClick={onCardClick}
-                />
-            ))}
-        </section>
-</main>
+                <button className="profile__add-button" onClick={() => handleAddPlaceClick(true)} type="button"></button>
+            </section>
+            <section className="elements">
+                {cards.map((card) => (
+                    <Card
+                        key={card.cardId}
+                        likes={card.likes}
+                        name={card.name}
+                        link={card.link}
+                        onCardClick={onCardClick}
+                    />
+                ))}
+            </section>
+        </main>
     );
 }
 
